a1840e70c0496914dc2a43e76235a62ab36f0b45
ale_figure8 <gomeznieto@gmail.com>
2022-05-27 16:34:35 -0300

    Adding save method

--- backend/container/usersContainer.js
+++ backend/container/usersContainer.js
@@ -1,5 +1,11 @@
-class Users {
+import Usuario from '../models/Usuario.js';
 
+class Users {
+  async saveUser(data){
+    const usuario = new Usuario(data);
+    const userSave = await usuario.save();
+    return userSave;
+  }
 }
 
 export default Users;
--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,5 +1,18 @@
-const registerUser = (req, res) => {
-  
+import User from '../container/usersContainer.js'
+
+/* Init Class */
+const usuario = new User()
+
+
+/* register User */
+const registerUser = async (req, res) => {
+  try {
+    const UserData = await usuario.saveUser(req.body);
+    res.json(UserData);
+  } catch (err){
+    console.log(`Error: ${err.messagge}`);
+  }
+
 }
 
 export {
