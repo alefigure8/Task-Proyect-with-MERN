fff73181471ad5ae9d6df6b18da052197cee0e80
ale_figure8 <gomeznieto@gmail.com>
2022-06-28 10:00:22 -0300

    task model mongoose

---
+++ backend/models/task.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const task = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  state: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  priority:{
+    type: String,
+    required: true,
+    enum: ['low', 'medium', 'high'],
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+  }
+}, {
+  timestamp: true,
+})
+
+const Task = mongoose.model('Task', task);
+
+export default Task;
