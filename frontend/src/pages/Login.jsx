444c0808b31625d766e95f2402f06bca25b85569
ale_figure8 <gomeznieto@gmail.com>
2022-06-30 11:07:16 -0300

    login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,38 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Login and Manage your
+        <span className="text-slate-700"> projects</span>
+      </h1>
+      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+        <div>
+          <label 
+            className="Uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+            >Email</label>
+          <input 
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            className="w-full mt-3 border rounded-xl bg-gray-50 border-sky-600 p-3"
+          />
+        </div>
+        <div className="my-5">
+          <label 
+            className="Uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+            >Password</label>
+          <input 
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            className="w-full mt-3 border rounded-xl bg-gray-50 border-sky-600 p-3 my-3"
+          />
+        </div>
+        <input type="submit" value="Login" className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-3" />
+      </form>
+    </>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", ".src/**/*.jsx"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "index.html",
+  ],
   theme: {
     extend: {},
   },
