8b34b832549f426cc9ba5e4dd51cd294dd9c9a02
ale_figure8 <gomeznieto@gmail.com>
2022-07-04 09:38:26 -0300

    CORS

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,25 @@ import dotenv from 'dotenv';
 import users from './routes/userRoutes.js';
 import projects from './routes/projectRoutes.js';
 import tasks from './routes/taskRoutes.js';
+import cors from 'cors';
 
 /* init */
 const app = express();
 app.set('PORT', process.env.PORT || 8080);
 dotenv.config()
 contectarDB()
+
+// CORS
+const whiteList = ['http://localhost:3000', 'http://localhost:8080'];
+const corsOptions = { origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      console.log('origin: ', origin);
+    } else {
+
+    }
+  }
+};
+app.use(cors(corsOptions));
 
 /* Middleware */
 app.use(express.json());
