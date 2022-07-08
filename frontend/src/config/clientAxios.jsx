66cb7ba7fb3cfab684425bedb9787e48c486b461
ale_figure8 <gomeznieto@gmail.com>
2022-07-08 11:12:58 -0300

    change url

--- frontend/src/config/clientAxios.jsx
+++ frontend/src/config/clientAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const clientAxios = axios.create({
-  baseURL: `${process.env.VITE_BACKEND_URL}/api`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 export default clientAxios;
