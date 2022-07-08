b97846abf85f2da6909cab448b827487f49d7047
ale_figure8 <gomeznieto@gmail.com>
2022-07-08 10:25:00 -0300

    adding login link to reset pass

--- frontend/src/pages/NewPass.jsx
+++ frontend/src/pages/NewPass.jsx
@@ -8,6 +8,7 @@ const NewPass = () => {
   const [alert, setAlert] = useState({})
   const [password, setPassword] = useState('')
   const [confirm, setConfirm] = useState(false)
+  const [passwordConfirmed, setPasswordConfirmed] = useState(false)
   const params = useParams();
   const {token} = params;
 
@@ -45,6 +46,7 @@ const NewPass = () => {
       const {data} = await axios.post(url, {password});
       setAlert({msg: data.msg, error: false})
       setPassword('');
+      setPasswordConfirmed(true);
     } catch (error) {
       setAlert({msg: error.response.data.msg, error: true})
     }
@@ -52,32 +54,31 @@ const NewPass = () => {
 
   return (
     <>
-    <h1 className="text-sky-600 font-black text-6xl capitalize">
-      Reset yourr password and do not lose access to your
-      <span className="text-slate-700"> projects</span>
-    </h1>
-    {alert?.msg && <Alert alert={alert}/>}
-    {confirm && <>
-      <form onSubmit={handleSubmit} className="my-10 bg-white shadow rounded-lg p-10">
-      <div className="my-3">
-        <label 
-          className="Uppercase text-gray-600 block text-xl font-bold"
-          htmlFor="password"
-          >New Password</label>
-        <input 
-          id="password"
-          type="password"
-          placeholder="Enter your new password"
-          className="w-full mt-3 border rounded-xl bg-gray-50 border-sky-600 p-3 my-3"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <input type="submit" value="Save new password" className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5" />
-    </form>
-    <div className="text-center"><Link to="/login">Go to login</Link></div>
-    </>}
-  </>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Reset yourr password and do not lose access to your
+        <span className="text-slate-700"> projects</span>
+      </h1>
+      {alert?.msg && <Alert alert={alert}/>}
+      {confirm && 
+        <form onSubmit={handleSubmit} className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-3">
+          <label 
+            className="Uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+            >New Password</label>
+          <input 
+            id="password"
+            type="password"
+            placeholder="Enter your new password"
+            className="w-full mt-3 border rounded-xl bg-gray-50 border-sky-600 p-3 my-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Save new password" className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5" />
+      </form> }
+      {passwordConfirmed && <div className="text-center"><Link to="/login">Login</Link></div>}
+    </>
   )
 }
 
