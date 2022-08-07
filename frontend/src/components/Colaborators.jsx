a231f0d1d736f0c94ca05f3bd4cf56cb8f9a86ee
ale_figure8 <gomeznieto@gmail.com>
2022-08-07 18:09:21 -0300

    add user & admin views

--- frontend/src/components/Colaborators.jsx
+++ frontend/src/components/Colaborators.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import useProjects from '../hooks/useProjects';
+import useAuth from '../hooks/useAuth';
+import useAdmin from '../hooks/useAdmin';
 
 const Colaborators = ({colaborator}) => {
   const {handleModalColaborator} = useProjects();
-  const {name, email} = colaborator;
+  const {name, email, _id} = colaborator;
+  const {auth} = useAuth();
+  const admin = useAdmin();
+
   return (
     <div className='border-b p-5 flex justify-between'>
       <div>
         <p className='font-bold text-gray-700'>{name}</p>
         <p className='text-gray-600 text-sm'>{email}</p>
       </div>
-      <button 
-            onClick={() => handleModalColaborator(colaborator)}
-            className='uppercase bg-red-600 px-4 py-2 font-bold text-sm text-white rounded-lg'
-          >
-            Delete
+     {auth._id === _id || admin ? (
+       <button 
+       onClick={() => handleModalColaborator(colaborator)}
+       className='uppercase bg-red-600 px-4 py-2 font-bold text-sm text-white rounded-lg'
+     >
+        Remove
       </button>
-
+     ):
+     <button 
+        disabled
+       className='uppercase bg-gray-400 px-4 py-2 font-bold text-sm text-gray-200 rounded-lg'
+     >
+        Remove
+      </button>
+    }
     </div>
   )
 }
