87b998f2e795b2b20bc1d1c48c136b9d203a993c
ale_figure8 <gomeznieto@gmail.com>
2022-08-07 18:28:00 -0300

    add label colaborator

--- frontend/src/components/Preview.jsx
+++ frontend/src/components/Preview.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const Preview = ({project}) => {
-  const {name, client, _id} = project
+  const {auth}  = useAuth()
+  const {name, client, _id, createdBy} = project
+  const isOwner = createdBy === auth._id
   return (
-    <div className='border-b border-gray-300 flex p-5'>
-      <p className='flex-1'>
-        {name}
-        <span className='flex-1 text-gray-400 uppercase'>{' '}{client}</span>
-      </p>
-      <Link to={`${_id}`} className=' text-gray-500 hover:text-gray-600 transition-colors font-bold text-sm uppercase'>got to project</Link>
+    <div className='border-b border-gray-300 flex p-5 justify-between'>
+      <div className='flex gap-2 justify-between'>
+        <p className='flex gap-2'>
+          {name}
+          <span className='flex-1 text-gray-400 uppercase'>{' '}{client}</span>
+        </p>
+          {!isOwner && 
+            <p className='py-1 px-2 bg-green-700 text-sm text-white font-bold uppercase rounded-md'>Colaborator</p>
+          }
+      </div>
+        <Link to={`${_id}`} className=' text-gray-500 hover:text-gray-600 transition-colors font-bold text-sm uppercase'>got to project</Link>
     </div>
   )
 }
